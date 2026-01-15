24e39c19cd8c0bf039e1a21f561bc982ea40b9ff
Janhjg <janhjanh93@gmail.com>
2026-01-15 12:19:33 +0100

    anadido test

---
+++ test/test_fizzbuzz.py
@@ -0,0 +1,39 @@
+def test_divisible_3():
+    #testea la funcion divisible entre 3 funcione
+    pass
+
+def test_divisible_5():
+    #testea que la funcion divisible entre 5 funcione
+    pass
+
+def test_divisible_3and5():
+    # testea que la funcion sea divisible entre 3 y 5 funcione
+    pass
+
+def test_no_divisible():
+    #testea cuando el numero no es divisible
+    pass
+
+def test_0():
+    # testea 0 es divisible entre 3 y 5
+    pass
+
+def test_fizz():
+    # de respuesta fizz 
+    pass
+
+def test_buzz():
+    # responda bien buzz
+    pass
+
+def test_fizzbuzz():
+    # responda bien fizzbuzz
+    pass
+
+def test_respuestanumero():
+    # devuelva correctamente el numero
+    pass
+
+def test_letrainput_error():
+    # cuando escriban texto de error
+    pass
